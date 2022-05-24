98795407a53329df9eee3a9fe0db0d00845d82f2
P2535230 <106113761+P2535230@users.noreply.github.com>
2022-05-24 12:03:30 +0100

    Update show_events.py

--- show_events.py
+++ show_events.py
@@ -1,11 +1,13 @@
+#importing the same libraries that the rest of the code uses
 from turtle import width
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from matplotlib import widgets
+from matplotlib import widgets 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 
+#presetting show event
 class show_events(QWindow):
     
     def showEvent(self, a0: QShowEvent) -> None:
