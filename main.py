2c31051a7fb5f51a6eb9c0809641984b4a5fcab3
P2535230 <106113761+P2535230@users.noreply.github.com>
2022-05-24 12:00:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from ast import Import
 import sys
 from PyQt5.QtWidgets import QApplication
 from CalendarAssistantGui import CalendarAssistantGui
+#calling callendarAssistantGui
+#to run the application
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
 
     App = QApplication(sys.argv)
     
+    #hardsetting the screensize
     window = CalendarAssistantGui(1280, 720)
     
     sys.exit(App.exec())
